e8c530f057ce0e0d144d40483760ae331579f6be
Xidgot <seanvs@hotmail.be>
2023-04-20 09:35:26 +0200

    changed hello world to hello sound gang

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -59,7 +59,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x53616263
+ #define JucePlugin_PluginCode             0x4d647739
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                0
